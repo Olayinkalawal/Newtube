27c21a4e027e962962b81a22a69d35b7045e0bff
Olayinkalawal <olayinkalawal@Olayinkas-Air.Home>
2025-02-08 10:15:12 +0000

    feat: implement clerk authentication - Add auth provider, middleware, sign-in/up pages, and protected routes

---
+++ src/app/(home)/protected/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>Only signed in users can see this page</div>;
+};
+
+export default page;
--- src/modules/home/ui/components/home-sidebar/main-section.tsx
+++ src/modules/home/ui/components/home-sidebar/main-section.tsx
@@ -8,6 +8,7 @@ import {
   SidebarMenuItem,
   SidebarMenuButton,
 } from '@/components/ui/sidebar';
+import { useAuth, useClerk } from '@clerk/nextjs';
 import Link from 'next/link';
 
 const items = [
@@ -30,6 +31,9 @@ const items = [
 ];
 
 export const MainSection = () => {
+  const { isSignedIn } = useAuth();
+  const clerk = useClerk();
+
   return (
     <SidebarGroup>
       <SidebarGroupContent>
@@ -40,7 +44,12 @@ export const MainSection = () => {
                 tooltip={item.title}
                 asChild
                 isActive={false}// TODO: Change to look at current pathname
-                onClick={() => {}}// TODO:Do Something on click
+                onClick={(e) => {
+                  if (!isSignedIn && item.auth) {
+                    e.preventDefault();
+                    return clerk.openSignIn();
+                  }
+                }}
               >
                 <Link href={item.url} className="flex items-center gap-4">
                   <item.icon />
